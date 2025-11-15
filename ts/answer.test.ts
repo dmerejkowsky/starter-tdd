fc0b39284210999cc526aa76f46371ab3f6c548d
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-11-15 10:49:25 +0100

    ts: switch to vitest

--- ts/answer.test.ts
+++ ts/answer.test.ts
@@ -1,9 +1,9 @@
-import getAnswer from './answer'
-import { describe, expect, test } from '@jest/globals'
+import getAnswer from "./answer";
+import { describe, test, expect } from "vitest";
 
-describe('getting answers', () => {
-  test('it returns 42', () => {
-    const res = getAnswer()
-    expect(res).toBe(42)
-  })
-})
+describe("getting answers", () => {
+  test("it returns 42", () => {
+    const res = getAnswer();
+    expect(res).toBe(42);
+  });
+});
